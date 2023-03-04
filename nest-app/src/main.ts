89fb2d41e1a113746b9c92ba047464594962124a
Tran Hoang Nam <k59tranhoangnam@gmail.com>
2023-03-04 17:15:55 +0300

    update: lab1

--- nest-app/src/main.ts
+++ nest-app/src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { env } from 'process';
 import { AppModule } from './app.module';
 
-import { env } from 'process'; //
+import * as dotenv from 'dotenv';
 
-import * as dotenv from 'dotenv'; //
-
-dotenv.config(); //
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(env.PORT); //
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('pug');
+
+  await app.listen(env.PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
